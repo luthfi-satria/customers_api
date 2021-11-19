0fd7e75f947a88dfc29ec82d7571e8a2afe4fd10
okipratama97 <okipratama97@gmail.com>
2021-11-19 12:55:34 +0700

    feat(customers-orders): added internal for orders

--- src/internal/internal.service.ts
+++ src/internal/internal.service.ts
@@ -7,6 +7,15 @@ export class InternalService {
   constructor(private readonly customerService: CustomersService) {}
 
   async getCustomer(id: string): Promise<ProfileDocument> {
-    return await this.customerService.findOneWithActiveAddresses(id);
+    return this.customerService.findOneWithActiveAddresses(id);
+  }
+
+  async getCustomersBulk(ids: any): Promise<any> {
+    try {
+      return this.customerService.getBulkCustomers(ids);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
 }
