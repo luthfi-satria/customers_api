cc192b9d706daa827b9af2a67b56a36178d21675
DESKTOP-3L8AFF1\Lenovo <saldhyyoga-akarinti>
2022-08-11 16:42:15 +0700

    fix/add get all customers api for create notification

--- src/internal/internal.controller.ts
+++ src/internal/internal.controller.ts
@@ -1,21 +1,17 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Logger,
-  Param,
-  Post,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get, Logger, Param, Query } from '@nestjs/common';
 import { ProfileDocument } from 'src/database/entities/profile.entity';
 import { InternalService } from './internal.service';
-import { GetCustomerBulkDto } from './get-customer-bulk.dto';
 
 @Controller('api/v1/internal')
 export class InternalController {
   constructor(private readonly internalService: InternalService) {}
 
   logger: Logger = new Logger();
+
+  @Get('customer-all')
+  async getAllCustomers(): Promise<ProfileDocument> {
+    return this.internalService.getAllCustomers();
+  }
 
   @Get('customers/bulk')
   async getCustomersBulk(@Query() data: any): Promise<ProfileDocument> {
@@ -25,12 +21,5 @@ export class InternalController {
   @Get('customers/:id')
   async getCustomer(@Param('id') id: string): Promise<ProfileDocument> {
     return this.internalService.getCustomer(id);
-  }
-
-  @Post('customers/bulk')
-  async getCustomers(
-    @Body() data: GetCustomerBulkDto,
-  ): Promise<ProfileDocument> {
-    return this.internalService.getCustomersBulk(data.ids);
   }
 }
