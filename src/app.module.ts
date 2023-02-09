ecff081c9e21deecee357cfe7052bfd5cac90a0f
luthfisr <luthfi.ramadhan@edot.id>
2023-02-09 15:31:17 +0700

    commenting new feature for tracing deployment error

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseService } from './database/database.service';
 import { CustomersModule } from './customers/customers.module';
-// import { HashModule } from './hash/hash.module';
 import { AddressModule } from './address/address.module';
 import { CommonModule } from './common/common.module';
 import { InternalModule } from './internal/internal.module';
 import { HttpModule } from '@nestjs/axios';
 import { AuthModule } from './auth/auth.module';
 import { ReportsModule } from './reports/reports.module';
-import { SsoModule } from './sso/sso.module';
-import { SeederModule } from './database/seeders/seeder.module';
+// import { SsoModule } from './sso/sso.module';
+// import { SeederModule } from './database/seeders/seeder.module';
 import { SettingModule } from './settings/setting.module';
 
 @Module({
@@ -22,16 +21,16 @@ import { SettingModule } from './settings/setting.module';
     TypeOrmModule.forRootAsync({
       useClass: DatabaseService,
     }),
-    SeederModule,
+    // SeederModule,
     CustomersModule,
     HttpModule,
-    SettingModule,
+    // SettingModule,
     AddressModule,
     CommonModule,
     InternalModule,
     AuthModule,
     ReportsModule,
-    SsoModule,
+    // SsoModule,
   ],
   controllers: [AppController],
   providers: [AppService],
